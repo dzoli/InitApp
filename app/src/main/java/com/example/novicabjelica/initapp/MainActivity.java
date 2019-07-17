2ced60726c88f6209fa08b55d34681b5d7d2f33b
dzoli <bje.no9@gmail.com>
2019-07-17 15:58:28 +0200

    pre test setup

--- app/src/main/java/com/example/novicabjelica/initapp/MainActivity.java
+++ app/src/main/java/com/example/novicabjelica/initapp/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.novicabjelica.initapp;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import javax.inject.Inject;
 
 import dagger.android.support.DaggerAppCompatActivity;
 
-public class MainActivity extends DaggerAppCompatActivity {
+public class MainActivity extends DaggerAppCompatActivity implements MainContract.View {
+    Button fab;
+    TextView textView;
 
     @Inject
     MainContract.Presenter presenter;
@@ -16,6 +20,17 @@ public class MainActivity extends DaggerAppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        fab = findViewById(R.id.fab);
+        textView = findViewById(R.id.textView);
 
+        fab.setOnClickListener(view -> {
+            presenter.createTopic(this);
+
+        });
+    }
+
+    @Override
+    public void showClickText() {
+        textView.setText("Fab was clicked!");
     }
 }
